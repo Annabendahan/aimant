655e1886810b788ba64843bfd07a9ae70b81ce9a
ThomasSignollet <Tames@macbook-pro-de-tames.home>
2020-06-11 10:50:39 +0200

    grid

---
+++ src/components/gridd.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
+
+export default function SpacingGrid() {
+  const [spacing, setSpacing] = React.useState(2);
+  const classes = useStyles();
+
+  const handleChange = (event) => {
+    setSpacing(Number(event.target.value));
+  };
+
+  return (
+    <Grid container className={classes.root} spacing={2}>
+      <Grid item xs={12}>
+        <Grid container justify="center" spacing={spacing}>
+          {[0, 1, 2].map((value) => (
+            <Grid key={value} item>
+              <Paper className={classes.paper} />
+           
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+      
+    </Grid>
+  );
+}
+
+
+export const pageQuery = graphql`
+  query thisQuery {
+    allWordpressPost(
+      sort: { fields: date, order: DESC }
+    ) {
+      
+      edges {
+        node {
+          id
+          title
+          
+          excerpt
+          
+          categories
+          path
+          _links {
+            wp_featuredmedia {
+              href
+              
+              embeddable
+            }
+          }
+          
+       
+          content
+         
+        }
+      }
+    }
+  }
+`

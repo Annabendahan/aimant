7fe3e53c09d318aa3e1c7e0407b186210630e0c6
david-gribouille <kevin@martin.best>
2020-12-28 17:55:10 +0100

    fix many things

--- src/templates/post.js
+++ src/templates/post.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import SimpleReactLightbox, { SRLWrapper } from 'simple-react-lightbox';
 import Vimeo from '@u-wave/react-vimeo';
+import YouTube from 'react-youtube';
 
 import contentParser from 'gatsby-wpgraphql-inline-images';
 import '../components/style.scss?version=4';
@@ -26,6 +27,10 @@ const options = {
   },
 };
 
+const replaceImage = (content) =>
+  content &&
+  content.replace(/http:\/\/aimantarwm.cluster021.hosting.ovh.net/gim, `https://cms.aimant.art`);
+
 const Post = (props) => {
   const {
     data: {
@@ -35,8 +40,13 @@ const Post = (props) => {
       pluginOptions: { wordPressUrl, uploadsUrl },
     },
   } = props;
-  const { title, content, acf } = post;
+
+  const { title, content = '', acf } = post;
+  const clearContent = replaceImage(content);
+
+  const postHaveBanner = clearContent.indexOf('banner') >= 0;
   const { video = '' } = acf || {};
+
   const customCaptions = Array(10)
     .fill(null)
     .map((item, index) => ({
@@ -44,34 +54,59 @@ const Post = (props) => {
       caption: <div className="caption">{title} &copy;</div>,
     }));
 
+  const isVimeo = video && video.indexOf('vimeo') >= 0;
+
+  const youtubeMatches =
+    !isVimeo && video && video.match(/^.*(youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\&\?]*).*/);
+  const youtubeId = youtubeMatches && youtubeMatches.length >= 1 ? youtubeMatches[2] : '';
   return (
     <SimpleReactLightbox>
       <React.Fragment>
         {/* Bannière */}
-        {props.data.wpgraphql.post.acf.bannertext && (
+        {props.data.wpgraphql.post.acf.bannerpicture.sourceUrl && (
           <div
             className="banner-container"
             style={{
               backgroundImage: `url(${props.data.wpgraphql.post.acf.bannerpicture.sourceUrl})`,
             }}
           >
-            <div className="nameBig">{props.data.wpgraphql.post.acf.bannertext}</div>
+            {!postHaveBanner && props.data.wpgraphql.post.acf.bannertext && (
+              <div className="nameBig">{props.data.wpgraphql.post.acf.bannertext}</div>
+            )}
           </div>
         )}
 
         {video && (
-          <Vimeo
-            className="artist-video"
-            video={video}
-            autplay={true}
-            autopause={true}
-            height="350"
-            width="800"
-          />
+          <React.Fragment>
+            {' '}
+            {isVimeo ? (
+              <Vimeo
+                className="artist-video"
+                video={video}
+                autplay={true}
+                autopause={true}
+                height="350"
+                width="800"
+              />
+            ) : (
+              <div className="artist-video">
+                <YouTube
+                  videoId={youtubeId}
+                  opts={{
+                    height: '390',
+                    width: '640',
+                    playerVars: {
+                      autoplay: 1,
+                    },
+                  }}
+                />
+              </div>
+            )}
+          </React.Fragment>
         )}
 
         <SRLWrapper options={options} customCaptions={customCaptions}>
-          {contentParser({ content }, { wordPressUrl, uploadsUrl })}
+          {contentParser({ content: clearContent }, { wordPressUrl, uploadsUrl })}
         </SRLWrapper>
       </React.Fragment>
       <p role="button" className="download" onClick={() => window.print()}>
